7fac2df238f73b9a2fc53e5edb265f3ef97aab7d
noahspott <noahspottdev@gmail.com>
2025-07-19 20:24:29 -0300

    feat: ContactDao

---
+++ app/src/main/java/com/noahspott/callyourmom/data/dao/ContactDao.kt
@@ -0,0 +1,19 @@
+package com.noahspott.callyourmom.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.noahspott.callyourmom.data.model.Contact
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ContactDao {
+    @Upsert
+    suspend fun upsertContact(contact: Contact)
+    @Delete
+    suspend fun  deleteContact(contact: Contact)
+
+    @Query("SELECT * FROM contact")
+    fun getAllContacts(): Flow<List<Contact>>
+}
