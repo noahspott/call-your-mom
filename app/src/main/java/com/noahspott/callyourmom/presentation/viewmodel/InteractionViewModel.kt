0ff3c4ebbc803b9fa18db13717da4339499a2fd9
noahspott <noahspottdev@gmail.com>
2025-07-23 18:15:43 -0300

    feat: add temp insert dummy data function

--- app/src/main/java/com/noahspott/callyourmom/presentation/viewmodel/InteractionViewModel.kt
+++ app/src/main/java/com/noahspott/callyourmom/presentation/viewmodel/InteractionViewModel.kt
@@ -25,4 +25,27 @@ class InteractionViewModel(private val repository: InteractionRepository): ViewM
             repository.delete(interaction)
         }
     }
+
+    fun insertDummyData() {
+        viewModelScope.launch {
+            val now = System.currentTimeMillis()
+            val dummyInteractions = listOf(
+                Interaction(id = 0, contactId = 1, timestamp = now - 100000000L), // 1+ days ago
+                Interaction(id = 0, contactId = 2, timestamp = now - 200000000L),
+                Interaction(id = 0, contactId = 3, timestamp = now - 300000000L),
+                Interaction(id = 0, contactId = 4, timestamp = now - 400000000L),
+                Interaction(id = 0, contactId = 5, timestamp = now - 500000000L),
+                Interaction(id = 0, contactId = 6, timestamp = now - 600000000L),
+                Interaction(id = 0, contactId = 7, timestamp = now - 700000000L),
+                Interaction(id = 0, contactId = 8, timestamp = now - 800000000L),
+                Interaction(id = 0, contactId = 9, timestamp = now - 900000000L),
+                Interaction(id = 0, contactId = 10, timestamp = now - 1000000000L),
+            )
+
+            dummyInteractions.forEach { interaction ->
+                repository.upsert(interaction)
+            }
+        }
+    }
+
 }
