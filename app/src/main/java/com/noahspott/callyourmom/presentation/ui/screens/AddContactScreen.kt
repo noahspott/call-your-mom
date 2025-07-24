9a6c24454a8a802e5b232c78c79677ce40b77c6e
noahspott <noahspottdev@gmail.com>
2025-07-24 15:35:59 -0300

    refactor: change screen name to just add contact

---
+++ app/src/main/java/com/noahspott/callyourmom/presentation/ui/screens/AddContactScreen.kt
@@ -0,0 +1,12 @@
+package com.noahspott.callyourmom.presentation.ui.screens
+
+import AppHeader
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.noahspott.callyourmom.presentation.ui.theme.CallYourMomTheme
+
+@Composable
+fun AddContactScreen() {
+    Text("Add Contact Screen")
+}
--- app/src/main/java/com/noahspott/callyourmom/presentation/ui/screens/AddEditContactScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.noahspott.callyourmom.presentation.ui.screens
-
