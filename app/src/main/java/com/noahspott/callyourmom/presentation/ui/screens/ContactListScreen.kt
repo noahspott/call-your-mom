417ba67f31a265e72523861fac73384a560fe582
noahspott <noahspottdev@gmail.com>
2025-07-28 16:06:59 -0300

    styles: add contact button - full width, easier to click

--- app/src/main/java/com/noahspott/callyourmom/presentation/ui/screens/ContactListScreen.kt
+++ app/src/main/java/com/noahspott/callyourmom/presentation/ui/screens/ContactListScreen.kt
@@ -3,9 +3,16 @@ package com.noahspott.callyourmom.presentation.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.noahspott.callyourmom.CallYourMomApplication
@@ -47,11 +55,12 @@ fun ContactListScreen(navController: NavController, modifier: Modifier) {
                 )
             }
         } else {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                IconButton(onClick = {navController.navigate(route = "addContactScreen")}) {
+            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                Button(colors = ButtonDefaults.textButtonColors(), modifier = Modifier.fillMaxWidth(), onClick = {navController.navigate(route = "addContactScreen")}) {
                     Icon(Icons.Filled.AddCircle, "Add a new contact")
+                    Spacer(Modifier.width(8.dp))
+                    Text("Add new contact")
                 }
-                Text("Add a new contact")
             }
             ContactCardList(contactCards, contactScreenViewModel::callButtonHandler)
         }
